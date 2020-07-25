42c0c6c4acf92f405324d6ac5605c4d8bfce48ae
Shashank Biplav <Biplavshashank7@gmail.com>
2020-07-25 22:55:35 +0530

    Added CRUD routes for a TODO API

--- routes/todos.ts
+++ routes/todos.ts
@@ -4,7 +4,7 @@ import {Todo} from '../models/todo'
 
 const router = Router();
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 router.get('/', (req, res, next) => {
     res.status(200).json({todos: todos});
@@ -16,6 +16,26 @@ router.post('/todo', (req, res, next)=>{
         text: req.body.text
     };
     todos.push(newTodo);
+    res.status(201).json({message:'Added Todo', todo:newTodo, todos:todos});
+});
+
+router.put('/todo/:todoId', (req, res, next)=>{
+    const todoId = req.params.todoId;
+    const todoIndex = todos.findIndex(todoItem => todoItem.id === todoId);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = {
+            id: todos[todoIndex].id,
+            text: req.body.text
+        };
+        return res.status(200).json({message:'Updated Todo', todos: todos});
+    }
+    res.status(404).json({ message: 'Todo not found'});
+});
+
+router.delete('/todo/:todoId', (req, res, next)=>{
+    const todoId = req.params.todoId;
+    todos = todos.filter(todoItem => todoItem.id !== todoId);
+    res.status(200).json({message:'Deleted todo', todos: todos});
 });
 
 export default router;
